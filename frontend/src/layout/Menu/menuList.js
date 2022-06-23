41b0fc4e37b98faeba119baa8fe1745e577b0b29
刃心 <king-ty@foxmail.com>
2022-06-23 16:08:16 +0800

    微调menu

--- frontend/src/layout/Menu/menuList.js
+++ frontend/src/layout/Menu/menuList.js
@@ -1,12 +1,17 @@
 export const menuList = [
   {
+    id: 125,
     authName: '用户管理',
-    children: {
-      id: 110,
-      authName: '用户列表',
-      path: 'users',
-      children: null,
-      order: 0
-    }
+    path: 'users',
+    children: [
+      {
+        id: 110,
+        authName: '用户列表',
+        path: 'users',
+        children: [],
+        order: null
+      }
+    ],
+    order: 1
   }
 ]
